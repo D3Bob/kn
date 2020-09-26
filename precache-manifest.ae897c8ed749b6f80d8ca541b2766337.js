cf0ee4ddf65c3b6f709c66a322104cdea10fe721
D3Bob <maks.rom.111@mail.ru>
2020-09-26 12:55:55 +0300

    Updates

---
+++ precache-manifest.ae897c8ed749b6f80d8ca541b2766337.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "68220a4de0ef5b49b99072376c80f635",
+    "url": "/kn/index.html"
+  },
+  {
+    "revision": "30deb3d6bd266cf16fbf",
+    "url": "/kn/static/css/main.13bff4b0.chunk.css"
+  },
+  {
+    "revision": "c6498ba1c504c433cdee",
+    "url": "/kn/static/js/2.24cf7c70.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/kn/static/js/2.24cf7c70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "30deb3d6bd266cf16fbf",
+    "url": "/kn/static/js/main.a8eadafa.chunk.js"
+  },
+  {
+    "revision": "fc4b05f3b38b09d8b42f",
+    "url": "/kn/static/js/runtime-main.ebe8168f.js"
+  }
+]);
